61ff30d1c76cd824b5bca46ad4e2fdc6ce8757ab
CYB292 <32217093+CYBYOB@users.noreply.github.com>
2018-11-06 09:08:28 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+//app.js
+App({
+  data: {
+
+  },
+
+  globalData: {
+    // 
+    nick_name: '陈yob_s',
+    user_id: '1_s',
+    avatar_url: 'https://wx.qlogo.cn/mmopen/vi_32/UtGsLfUtgdKSiaGgWVz2yOeCiamEia93cCDaa1WrbE7INIsSklkPAoZxa4ibiaIMgO2yNJhoyzDkqa15bkBbYKoYiaJQ/132_s',
+    openid: 0,
+    wx_url_1: 'https://api.weixin.qq.com/sns/jscode2session?appid=wx468a8bf22a59ac97&secret=5a16380f1419fc6fc941e651d71201a9&js_code=',
+    wx_url_2: '&grant_type=authorization_code'
+  },
+
+  /**
+   * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+   */
+  onLaunch: function () {
+    
+  },
+
+  /**
+   * 当小程序启动，或从后台进入前台显示，会触发 onShow
+   */
+  onShow: function (options) {
+    
+  },
+
+  /**
+   * 当小程序从前台进入后台，会触发 onHide
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
+   */
+  onError: function (msg) {
+    
+  }
+})
